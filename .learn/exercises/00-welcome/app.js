516445b84d513ea74de5f5f61257b6ed6296e01c
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-19 00:14:01 +0000

    build convert functions

--- .learn/exercises/00-welcome/app.js
+++ .learn/exercises/00-welcome/app.js
@@ -6,6 +6,38 @@ const fromEuroToDollar = function(valueInEuro) {
     return valueInDollar;
 }
 
+// from Dollar to Yen
+const fromDOllarToYen = function(valueInDollar){
+// Convert the given valueInDollar to Yen
+let valueInYen = valueInDOllar * 156.5
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromYenToPound = function(valueInEurYen) {
+    // Convert the given valueInYen to pounds
+    let valueInPounds = valueInEuro * 0.87;
+    // return the dollar value
+    return valueInPounds;
+}
+
+// Euro to Yen
+const fromDOllarToYen = function(valueInEuro){
+// Convert the given valueInEuro to Yen
+let valueInYen = valueInEuro * 156.5
+}
+
+// Euro to Yen
+const fromDOllarToYen = function(valueInEuro){
+// Convert the given valueInEuro to Yen
+let valueInYen = valueInEuro * 156.5
+}
+
 // This is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
